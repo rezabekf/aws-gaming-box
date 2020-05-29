a5a6827824db88c567378e4d797d3d2ba1ef0227
Franco Rezabek <rezabekf@amazon.co.uk>
2020-05-29 14:55:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# AWS G4 Gaming Box
+
+## Pre-req
+1. Install `AWS CLI`
+1. Install `pip`
+
+## Deploy the CloudFormation Stack
+As the solution is using `Lambda` function, the code needs to be zipped and uploaded to S3 bucket. The `deploy.sh` script will install lambda libraries, will package the template to S3 bucket and finaly will deploy the CloudFormation stack.
+
+1. Create Amazon S3 bucket
+   ```
+   $ BUCKET_NAME=""
+   $ AWS_REGION=""
+   $ aws s3 mb s3://${BUCKET_NAME} --region $AWS_REGION
+   ```
+1. Create Amazon EC2 key pair
+   Replace the `--key-name` with your own key name and specify the location after `>`
+   ```
+   aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > ~/Downloads/MyKeyPair.pem --region $AWS_REGION
+   ```
+1. Set the permission of the `.pem` file to read only
+   ```
+   chmod 400 ~/Downloads/MyKeyPair.pem
+   ```
+1. Create an `.env` file and populate it with your own values
+   
+   ```
+   $ cp .env.example .env
+   ```
+   
+   |Variable Label|Example|Description|
+   |--------------|-------|-----------|
+   |BUCKET_NAME|my-unique-bucket-name|Use the same value from step above|
+   |PREFIX_NAME|gaming-box-infra|This will put all the code in a bucket folder|
+   |STACK_NAME |GamingBox|The name of the stack|
+   |AWS_REGION |eu-west-2|The AWS region to deploy the solution to|
+   |KEY_PAIR   |MyKeyPair|The name of the KeyPair created above|
+   |ON_PREM_IP |0.0.0.0/0|CIDR annotation of your home IP, to increase the security|
+   
+1. Double check that `deploy.sh` is executable, if not run:
+   ```
+   $ chmod +x deploy.sh
+   ```
+1. Run the deployment script
+   ```
+   $ ./deploy.sh
+   ```
+1. Go to AWS console and wait for the stack to complete
+1. Navigate to the EC2 console, select the Gaming instance and click on **Connect**
+1. Note down the **Public DNS**, **User name** and **Password** (you may have to wait a few minutes for the password to become available)
+
+## Connect to the Gaming Server
+1. Get NICE DCV Client from [https://docs.aws.amazon.com/dcv/latest/userguide/client.html](https://docs.aws.amazon.com/dcv/latest/userguide/client.html)
+1. Connect via DCV client, providing login details noted above
+
+## Install Parsec
+1. Open PowerShell and paste command bellow:
+```powershell
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"  
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/jamesstringerparsec/Parsec-Cloud-Preparation-Tool/archive/master.zip","$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.zip")  
+New-Item -Path $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool -ItemType Directory  
+Expand-Archive $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.Zip -DestinationPath $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool  
+CD $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\  
+Powershell.exe -File $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\Loader.ps1
+
+```
+1. Follow installation instructions from Parsec Team blog post [RTX Cloud Gaming With The New AWS G4 Instances](https://blog.parsecgaming.com/rtx-cloud-gaming-with-the-new-aws-g4-instances-11d1c60c2d09)
+
+## What will happen when you are done gaming?
---
+++ snapshotter/app.py
@@ -0,0 +1,112 @@
+import os
+import boto3
+
+from botocore import exceptions
+
+gaming_instance_name = os.environ['GAMING_INSTANCE_NAME']
+gaming_instance_region = os.environ['GAMING_INSTANCE_REGION']
+gaming_instance_size_gb = os.environ['GAMING_INSTANCE_SIZE_GB']
+
+# Connect to region
+ec2_client = boto3.client('ec2', region_name=gaming_instance_region)
+ec2_resource = boto3.resource('ec2', region_name=gaming_instance_region)
+
+
+def lambda_handler(event, context):
+    # Get all available volumes
+    volumes = ec2_client.describe_volumes(Filters=[{'Name': 'status', 'Values': ['available']}])['Volumes']
+
+    # Get all volumes for the given instance
+    volumes_to_delete = []
+    for volume in volumes:
+        for tag in volume['Tags']:
+            if tag['Key'] == 'Name' and tag['Value'] == gaming_instance_name:
+                volumes_to_delete.append(volume)
+
+    if len(volumes_to_delete) == 0:
+        print('No volumes found. Nothing to do! Aborting...')
+        return
+
+    # Create a snapshot of the volumes
+    snaps_created = []
+    for volume in volumes:
+        snap = ec2_client.create_snapshot(VolumeId=volume['VolumeId'])
+        snap_id = snap['SnapshotId']
+        snap_waiter = ec2_client.get_waiter('snapshot_completed')
+
+        try:
+            snap_waiter.wait(SnapshotIds=[snap_id], WaiterConfig={'Delay': 15, 'MaxAttempts': 59})
+        except exceptions.WaiterError as e:
+            print("Could not create snapshot, aborting")
+            print(e.last_response)
+            return
+
+        print("Created snapshot: {}".format(snap['SnapshotId']))
+        snaps_created.append(snap['SnapshotId'])
+
+    # Tag the snapshots
+    if len(snaps_created) > 0:
+        ec2_client.create_tags(
+            Resources=snaps_created,
+            Tags=[
+                {'Key': 'SnapAndDelete', 'Value': 'True'},
+                {'Key': 'Name', 'Value': gaming_instance_name}
+            ]
+        )
+
+    # Delete any current AMIs
+    images = ec2_client.describe_images(Owners=['self'])['Images']
+    for ami in images:
+        if ami['Name'] == gaming_instance_name:
+            print('Deleting image {}'.format(ami['ImageId']))
+            ec2_client.deregister_image(DryRun=False, ImageId=ami['ImageId'])
+
+    # Remove previous snapshots of the volumes
+    previous_snapshots = ec2_client.describe_snapshots(Filters=[{'Name': 'tag-key', 'Values': ['SnapAndDelete']}])[
+        'Snapshots']
+    for snapshot in previous_snapshots:
+        if snapshot['SnapshotId'] not in snaps_created:
+            print("Removing previous snapshot: {}".format(snapshot['SnapshotId']))
+            ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+
+    # Delete the volumes
+    for volume in volumes_to_delete:
+        v = ec2_resource.Volume(volume['VolumeId'])
+        print("Deleting EBS volume: {}, Size: {} GiB".format(v.id, v.size))
+        v.delete()
+
+    # Create a new AMI
+    if len(snaps_created) > 0:
+        amis_created = []
+        ami = ec2_client.register_image(
+            Name=gaming_instance_name,
+            Description=gaming_instance_name + ' Automatic AMI',
+            BlockDeviceMappings=[
+                {
+                    'DeviceName': '/dev/sda1',
+                    'Ebs': {
+                        'DeleteOnTermination': False,
+                        'SnapshotId': snaps_created[0],
+                        'VolumeSize': int(gaming_instance_size_gb),
+                        'VolumeType': 'gp2'
+                    }
+                },
+            ],
+            Architecture='x86_64',
+            RootDeviceName='/dev/sda1',
+            DryRun=False,
+            VirtualizationType='hvm',
+            SriovNetSupport='simple'
+        )
+        print('Created image {}'.format(ami['ImageId']))
+        amis_created.append(ami['ImageId'])
+
+        if len(amis_created) > 0:
+            # Tag the AMI
+            ec2_client.create_tags(
+                Resources=amis_created,
+                Tags=[
+                    {'Key': 'SnapAndDelete', 'Value': 'True'},
+                    {'Key': 'Name', 'Value': gaming_instance_name}
+                ]
+            )
